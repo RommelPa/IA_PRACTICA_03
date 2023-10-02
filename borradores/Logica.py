336cf7c9b1ffb1118892d86b89b7baad76ab633b
Rommel Paredes Banda <91158430+RommelPa@users.noreply.github.com>
2023-10-02 08:45:56 -0500

    Update and rename Logica.py to borradores/Logica.py

--- borradores/Logica.py
+++ borradores/Logica.py
@@ -1,6 +1,4 @@
 import pygame
-import random
-import copy
 
 def direcciones(x, y, minX=0, minY=0, maxX=7, maxY=7):
     direccionesValidas = []
@@ -109,7 +107,7 @@ class Tablero:
         
     def cargarImagenesFondo(reversi):
         alpha = 'ABCDEFGHI'
-        hojaSprites = pygame.image.load('assets/mapa.png').convert_alpha()
+        hojaSprites = pygame.image.load('assets/Mapa.png').convert_alpha()
         imagenDiccionario = {}
         for i in range(3):
             for j in range(7):
